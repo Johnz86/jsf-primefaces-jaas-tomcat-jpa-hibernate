e2d5a4a342fe3f0fed9c6448e1a8ff18648521f7
Johnz86 <jakubcikjan@gmail.com>
2016-08-17 17:22:13 +0200

    Change login mechanism to database source configured with Realm in context.xml.

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -9,3 +9,15 @@ INSERT INTO EMPLOYEE (ID, NAME, AGE, COMPANY) VALUES
 (2, 'bbb', 50, 2),
 (3, 'ccc', 30, 2),
 (4, 'ddd', 20, 1);
+
+INSERT INTO USERS (USER_NAME, USER_PASS) VALUES
+('root','0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33'),
+('john','0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33'),
+('fero','0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33');
+
+
+INSERT INTO USER_ROLES (USER_NAME, ROLE_NAME) VALUES
+('root','admin'),
+('john','user'),
+('fero','admin'),
+('fero','user');
