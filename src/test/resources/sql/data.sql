831a402ef6874aae84de89066ddde63384605af4
Jakubcik Jan <jakubcik.jan@scheidt-bachmann.sk>
2016-09-05 14:54:36 +0200

    Update persistence hibernate provider.

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -1,14 +1,16 @@
 -- DADOS USADOS NO AMBIENTE DE TESTES
 
 INSERT INTO COMPANY (ID, NAME) VALUES
-(1, 'Yo'),
-(2, 'What');
+(1, 'Test s.r.o.'),
+(2, 'Deutsch GMBH'),
+(3, 'PS Consultation'),
+(4, 'Webus');
 
 INSERT INTO EMPLOYEE (ID, NAME, AGE, COMPANY) VALUES
-(1, 'aaa', 40, 1),
-(2, 'bbb', 50, 2),
-(3, 'ccc', 30, 2),
-(4, 'ddd', 20, 1);
+(1, 'Jan', 40, 1),
+(2, 'Henry', 50, 2),
+(3, 'Adam', 30, 2),
+(4, 'Silvia', 20, 1);
 
 INSERT INTO USERS (USER_NAME, USER_PASS) VALUES
 ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),
